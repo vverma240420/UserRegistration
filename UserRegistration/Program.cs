ccb4aede0effa17dbccf4df73ebd538df3aeef34
Vivek Verma <vverma240420@gmail.com>
2023-04-03 09:37:10 +0530

    Added validation for first name

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace UserRegistration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Registration Program");
+            UserValidation.ValidateFirstName();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UserRegistration/UserValidation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class UserValidation
+    {
+        public static void ValidateFirstName()
+        {
+            Console.WriteLine("First Name: ");
+            string pattern = "^[A-Z]{1,}[a-z]{2,}$";
+            string input = Console.ReadLine();
+            ValidatePattern(input, pattern);
+        }
+        public static void ValidatePattern(string input, string pattern)
+        {
+            if (Regex.IsMatch(input, pattern))
+            {
+                Console.WriteLine("{0} is valid ", input);
+            }
+            else
+            {
+                Console.WriteLine("{0} is invalid ", input);
+            }
+        }
+    }
+}
